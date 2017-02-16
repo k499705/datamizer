998475f6ba8b6b02036fcc49eb8fb9429c0eae7b
ammgc <ammgc@LAPTOP-9HNFKU4F>
2017-02-16 10:19:11 +0100

    Algoritmo estándar y Niveles Interface 1

---
+++ Datanon/src/main/java/com/datanon/clasesestandar/Niveles.java
@@ -0,0 +1,19 @@
+/*
+ *     ___      _                          
+ *    /   \__ _| |_ __ _ _ __   ___  _ __  
+ *    / /\ / _` | __/ _` | '_ \ / _ \| '_ \ 
+ *  / /_// (_| | || (_| | | | | (_) | | | |
+ *  /___,' \__,_|\__\__,_|_| |_|\___/|_| |_|
+ *                                          
+ */
+package com.datanon.clasesestandar;
+
+/**
+ *
+ * @author ammgc
+ */
+public interface Niveles {
+    
+        public enum Nivel{ LEVE, MEDIO, ALTO};
+        
+}

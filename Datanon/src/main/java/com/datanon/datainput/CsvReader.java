b2a013ae4008559198027f94053319a573979bb4
Owner <Owner@->
2017-05-06 19:10:25 +0200

    Merged

---
+++ Datanon/src/main/java/com/datanon/datainput/CsvReader.java
@@ -0,0 +1,33 @@
+/*
+ *     ___      _                          
+ *    /   \__ _| |_ __ _ _ __   ___  _ __  
+ *    / /\ / _` | __/ _` | '_ \ / _ \| '_ \ 
+ *  / /_// (_| | || (_| | | | | (_) | | | |
+ *  /___,' \__,_|\__\__,_|_| |_|\___/|_| |_|
+ *                                          
+ */
+package com.datanon.datainput;
+
+import com.opencsv.CSVReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+
+/**
+ *
+ * @author ammgc
+ */
+public class CsvReader {
+
+    private static final char SEPARATOR = ',';
+    
+    public static List<String[]> csvToSignalDataList(String filepath) throws IOException {
+               
+            CSVReader reader = new CSVReader(new FileReader(filepath), SEPARATOR);
+            List<String[]> rows = reader.readAll(); 
+            return rows;
+        
+    }
+    
+}
